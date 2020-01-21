a086ca515b8804dc469a8370a33cbbc220ff6f6e
Siranjiv <siranjiv@gmail.com>
2020-01-21 17:51:06 +0530

    Lab 2 works

---
+++ L2/exercise3.java
@@ -0,0 +1,9 @@
+class exercise3
+{
+    public static void main(String[] args)
+    {
+        int a=Integer.parseInt(args[0]);
+        System.out.println("Squareroot"+Math.sqrt(a));
+        System.out.println("Sqare"+Math.pow(a,2));
+    }
+}
---
+++ L2/exercise4.java
@@ -0,0 +1,7 @@
+class exercise4
+{
+    public static void main(String[] args)
+    {
+        int radius =Integer.parseInt(args[0]);
+    }
+}
